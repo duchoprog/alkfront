81e4461db440e7f228d3af4bf97811bb484f7e7e
pabloduchovny <pablo.duchovny@gmail.com>
2021-01-24 16:47:28 -0300

    added Op.js

---
+++ src/components/Op.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Op = (props) => {
+    return (
+        <tr className='op'>
+            <td>{props.categoria}</td>
+            <td>{props.concepto}</td>
+            <td>{props.monto}</td>
+            <td>{props.fecha}</td>
+            <td>{props.tipo}</td>
+
+        </tr>
+    );
+}
+export default Op;
