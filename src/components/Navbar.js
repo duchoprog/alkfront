23f638686b5026d502a45b8f763df074b3516f73
pabloduchovny <pablo.duchovny@gmail.com>
2021-01-24 12:33:25 -0300

    added Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+const Navbar = ({ title }) => {
+
+    return (
+        <nav className="navbar  bg-primary">
+            <h1>
+                {title}
+            </h1>
+            <ul>
+                <li>
+                    <Link to=''>Operar</Link>
+                </li>
+                <li>
+                    <Link to=''>Home</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+
+}
+Navbar.defaultProps = {
+    title: 'Gastos-Ingresos'
+};
+export default Navbar
