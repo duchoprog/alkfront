8f7d326e3c8a51b6a959b41f223dbbeffac8dd71
pabloduchovny <pablo.duchovny@gmail.com>
2021-01-28 11:41:58 -0300

    Fixed monto and fecha display format in Op.js

--- src/components/Op.js
+++ src/components/Op.js
@@ -3,10 +3,10 @@ const Op = (props) => {
     return (
         <tr className='op' key={props.id}>
             <td><button name={props.id}>modificar</button>
-            <button name={props.id}>eliminar</button></td>
+                <button name={props.id}>eliminar</button></td>
             <td>{props.concepto}</td>
-            <td>{props.monto}</td>
-            <td>{props.fecha}</td>
+            <td>{props.monto.toFixed(2)}</td>
+            <td>{props.fecha.substr(0, 10)}</td>
             <td>{props.tipo}</td>
 
         </tr>
